cf10fbc55da4d147be6fd0c251f20752b1ac3b7b
Rahul Shetty <shettyrahul8june@gmail.com>
2024-04-17 08:07:28 +0530

    feat: handle issue related to bundling the project

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,30 @@ import initializeMiddlewares from '@/middleware';
 const app: Express = express();
 const httpServer = http.createServer(app);
 
-try {
-  await initializeMiddlewares(app, httpServer);
+const startServer = async () => {
+  try {
+    await initializeMiddlewares(app, httpServer);
 
-  const server = httpServer.listen(env.PORT, () => {
-    const { NODE_ENV, HOST, PORT } = env;
-    console.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
-  });
-
-  // Graceful shutdown
-  const onCloseSignal = () => {
-    console.info('sigint received, shutting down');
-    server.close(() => {
-      console.info('server closed');
-      process.exit();
+    const server = httpServer.listen(env.PORT, () => {
+      const { NODE_ENV, HOST, PORT } = env;
+      console.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
     });
-    setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
-  };
 
-  process.on('SIGINT', onCloseSignal);
-  process.on('SIGTERM', onCloseSignal);
-} catch (error) {
-  console.error('Error initializing middlewares', error);
-}
+    // Graceful shutdown
+    const onCloseSignal = () => {
+      console.info('sigint received, shutting down');
+      server.close(() => {
+        console.info('server closed');
+        process.exit();
+      });
+      setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+    };
+
+    process.on('SIGINT', onCloseSignal);
+    process.on('SIGTERM', onCloseSignal);
+  } catch (error) {
+    console.error('Error initializing middlewares', error);
+  }
+};
+
+startServer();
